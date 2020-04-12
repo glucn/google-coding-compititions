3d7bed7f17d846a4edc5066375e6971ef2536f0a
Gary Lu <madcated.lu@gmail.com>
2020-04-12 15:04:12 -0700

    01 Pattern Matching - after checking the offical analysis

--- code_jam_2020/round_1a/01_Pattern_Matching/solution.go
+++ code_jam_2020/round_1a/01_Pattern_Matching/solution.go
@@ -8,69 +8,79 @@ import (
 	"strings"
 )
 
-// This was the code I submitted during round 1a, it only sovled the problem for test set 1
-// TODO: clean up the code
-// TODO: solve the problem for test set 2 & 3
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	var T int
 	if scanner.Scan() {
-		T = getInt(scanner.Text())
+		T, _ = strconv.Atoi((scanner.Text()))
 	}
 
 	for t := 0; t < T; t++ {
 		var N int
 		if scanner.Scan() {
-			N = getInt(scanner.Text())
+			N, _ = strconv.Atoi((scanner.Text()))
 		}
 
 		patterns := make([]string, N)
 
-		for n := 0; n<N; n++ {
+		for n := 0; n < N; n++ {
 			if scanner.Scan() {
 				patterns[n] = scanner.Text()
 			}
 		}
 
-		if ok, result := hasSameSuffix(patterns); ok {
-			fmt.Printf("Case #%d: %s\n", t+1, result)
-		} else {
-			fmt.Printf("Case #%d: %s\n", t+1, "*")
+		prefixs := make([]string, N)
+		suffixs := make([]string, N)
+		mids := make([]string, N)
+
+		for i, p := range patterns {
+			sections := strings.Split(p, "*")
+			prefixs[i] = sections[0]
+			suffixs[i] = sections[len(sections)-1]
+
+			if len(sections) > 2 {
+				mids[i] = strings.Join(sections[1:len(sections)-1], "")
+			}
 		}
 
-	
+		var prefix, suffix string
+
+		if p, ok := findSharedPattern(prefixs, strings.HasPrefix); ok {
+			prefix = p
+		} else {
+			fmt.Printf("Case #%d: %s\n", t+1, "*")
+			continue
+		}
+
+		if s, ok := findSharedPattern(suffixs, strings.HasSuffix); ok {
+			suffix = s
+		} else {
+			fmt.Printf("Case #%d: %s\n", t+1, "*")
+			continue
+		}
+
+		fmt.Printf("Case #%d: %s\n", t+1, prefix+strings.Join(mids, "")+suffix)
+
 	}
 }
 
-func getInt(s string) int {
-	n, err := strconv.Atoi(s)
-	if err != nil {
-		panic(err.Error())
-	}
-	return n
-}
-
-func hasSameSuffix(patterns []string) (bool, string) {
-
-	
-	for i := 0; i<len(patterns); i++ {
-		s1 := patterns[i][1:]
-		for j := 0; j<len(patterns); j++ {
-			s2 := patterns[j][1:]
-			if !strings.HasSuffix(s1, s2) && !strings.HasSuffix(s2, s1) {
-				return false, ""
+func findSharedPattern(patterns []string, checker func(s1, s2 string) bool) (string, bool) {
+	for i := 0; i < len(patterns); i++ {
+		for j := 0; j < len(patterns); j++ {
+			if !checker(patterns[i], patterns[j]) && !checker(patterns[j], patterns[i]) {
+				return "", false
 			}
 		}
 	}
 
-	maxPatternIndex := -1
+	maxIndex := -1
 	maxLen := -1
 	for i, p := range patterns {
 		if len(p) > maxLen {
 			maxLen = len(p)
-			maxPatternIndex = i
+			maxIndex = i
 		}
 	}
-	return true, patterns[maxPatternIndex][1:]
+	return patterns[maxIndex], true
 }
